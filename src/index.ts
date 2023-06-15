4304d9bca28e2392f44c887ec150f70aabf208c3
Sean Yasnogrodski <sean.yasno@gmail.com>
2023-06-15 13:14:45 +0300

    Added couple packages.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import helmet from 'helmet';
 
 const application =  express();
 
+application.use(morgan('common'));
+application.use(helmet());
 application.use(cors());
+application.use(express.json());
 
 application.get('/', (request, response) => {
     response.send('Hello World');
